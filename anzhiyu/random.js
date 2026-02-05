499b0f80162b9107746e3801c41afafb67ff4d50
liyitao <2180796307@qq.com>
2026-02-05 23:06:01 +0800

    Site updated: 2026-02-05 23:05:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/05/hello-world/","2026/02/05/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2017/01/23/这是一篇新的博文/","2026/02/05/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
