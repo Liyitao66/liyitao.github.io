3fcfe7315f30ae9834bc727352d960bec1a50513
liyitao <2180796307@qq.com>
2026-02-06 19:22:10 +0800

    Site updated: 2026-02-06 19:22:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2017/01/23/这是一篇新的博文/","2026/02/05/hello-world/"];function toRandomPost(){
+var posts=["2026/02/06/一元二次函数、方程和不等式 - 能力卷/","2026/02/06/集合与常用逻辑用语 - 能力卷 答案及解题过程/","2026/02/06/英语/","2026/02/06/1集合与常用逻辑用语-基础卷/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
