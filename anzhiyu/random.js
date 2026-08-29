a4e7248d6566c80f18fc31bc57855bc4e3aa9582
liyitao <2180796307@qq.com>
2026-08-29 13:41:21 +0800

    Site updated: 2026-08-29 13:41:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/29/开学时间/","2026/05/11/化学会考操作视频/"];function toRandomPost(){
+var posts=["2026/05/11/化学会考操作视频/"];function toRandomPost(){
     window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
